f7703b7e300a2bfcefe4ebb4a7d898a73354b959
Maximiliano Curia <maxy@gnuservers.com.ar>
2022-06-24 14:20:19 +0200

    CR is no longer set to select=true

--- nvim/lua/custom/configs/cmp.lua
+++ nvim/lua/custom/configs/cmp.lua
@@ -1,4 +1,6 @@
-local present, cmp = pcall(require, "cmp")
+local present, cmp_defaults = pcall(require, "cmp.config.default")
+
+vim.g.cmp_toggle_flag = true
 
 local M = {}
 
@@ -6,25 +8,10 @@ if not present then
    return M
 end
 
-M.mapping = {
-  ["<CR>"] = cmp.mapping.confirm {
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = false,
-  },
-}
-
-vim.g.cmp_toggle_flag = true
-
-local default_present, default = pcall(require, "cmp.config.default")
-
-if not default_present then
-   return M
-end
-
-local default_config = default()
+local cmp_config = cmp_defaults()
 
 M.enabled = function()
-  return default_config.enabled() and vim.g.cmp_toggle_flag
+  return cmp_config.enabled() and vim.g.cmp_toggle_flag
 end
 
 return M
