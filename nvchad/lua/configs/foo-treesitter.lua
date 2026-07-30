151c35889b719ade8f1a4f242b914a0593f5afc0
Maximiliano Curia <maxy@gnuservers.com.ar>
2026-07-30 18:02:55 +0200

    Local changes

--- nvchad/lua/configs/foo-treesitter.lua
+++ nvchad/lua/configs/foo-treesitter.lua
@@ -1,86 +1,86 @@
-local M = {
-  ensure_installed = {
-    "vim",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "json",
-    "toml",
-    "markdown",
-    "markdown_inline",
-    "c",
-    "bash",
-    "lua",
-  },
-
-  indent = {
-    enable = true,
-    disable = { "python", "css" },
-  },
-
-  autotag = {
-    enable = true,
-  },
-
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn", -- set to `false` to disable one of the mappings
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
-  textobjects = {
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]]"] = "@function.outer",
-        ["]m"] = "@class.outer",
-      },
-      goto_next_end = {
-        ["]["] = "@function.outer",
-        ["]M"] = "@class.outer",
-      },
-      goto_previous_start = {
-        ["[["] = "@function.outer",
-        ["[m"] = "@class.outer",
-      },
-      goto_previous_end = {
-        ["[]"] = "@function.outer",
-        ["[M"] = "@class.outer",
-      },
-    },
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
-        ["ii"] = "@conditional.inner",
-        ["ai"] = "@conditional.outer",
-        ["il"] = "@loop.inner",
-        ["al"] = "@loop.outer",
-        ["at"] = "@comment.outer",
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
-    },
-  },
-}
-return M
+-- local M = {
+--   ensure_installed = {
+--     "vim",
+--     "html",
+--     "css",
+--     "javascript",
+--     "typescript",
+--     "tsx",
+--     "json",
+--     "toml",
+--     "markdown",
+--     "markdown_inline",
+--     "c",
+--     "bash",
+--     "lua",
+--   },
+--
+--   indent = {
+--     enable = true,
+--     disable = { "python", "css" },
+--   },
+--
+--   autotag = {
+--     enable = true,
+--   },
+--
+--   incremental_selection = {
+--     enable = true,
+--     keymaps = {
+--       init_selection = "gnn", -- set to `false` to disable one of the mappings
+--       node_incremental = "grn",
+--       scope_incremental = "grc",
+--       node_decremental = "grm",
+--     },
+--   },
+--   textobjects = {
+--     move = {
+--       enable = true,
+--       set_jumps = true, -- whether to set jumps in the jumplist
+--       goto_next_start = {
+--         ["]]"] = "@function.outer",
+--         ["]m"] = "@class.outer",
+--       },
+--       goto_next_end = {
+--         ["]["] = "@function.outer",
+--         ["]M"] = "@class.outer",
+--       },
+--       goto_previous_start = {
+--         ["[["] = "@function.outer",
+--         ["[m"] = "@class.outer",
+--       },
+--       goto_previous_end = {
+--         ["[]"] = "@function.outer",
+--         ["[M"] = "@class.outer",
+--       },
+--     },
+--     select = {
+--       enable = true,
+--       lookahead = true,
+--       keymaps = {
+--         -- You can use the capture groups defined in textobjects.scm
+--         ["aa"] = "@parameter.outer",
+--         ["ia"] = "@parameter.inner",
+--         ["af"] = "@function.outer",
+--         ["if"] = "@function.inner",
+--         ["ac"] = "@class.outer",
+--         ["ic"] = "@class.inner",
+--         ["ii"] = "@conditional.inner",
+--         ["ai"] = "@conditional.outer",
+--         ["il"] = "@loop.inner",
+--         ["al"] = "@loop.outer",
+--         ["at"] = "@comment.outer",
+--       },
+--     },
+--     swap = {
+--       enable = true,
+--       swap_next = {
+--         ["<leader>a"] = "@parameter.inner",
+--       },
+--       swap_previous = {
+--         ["<leader>A"] = "@parameter.inner",
+--       },
+--     },
+--   },
+-- }
+-- return M
